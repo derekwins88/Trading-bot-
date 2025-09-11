f3d017bfa4d34989c5ed3bd54864a5b5df313beb
Derek Alexander Espinoza <derekespinoza88@gmail.com>
2025-09-11 10:20:14 -0700

    Add execution modules

---
+++ src/Strategy.cs
@@ -0,0 +1,8 @@
+namespace SymbolicTrading
+{
+    public class Strategy
+    {
+        public void EnterLong(int quantity, string signalName) { }
+        public void EnterShort(int quantity, string signalName) { }
+    }
+}
